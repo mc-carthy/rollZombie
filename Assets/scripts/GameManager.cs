7402fb62f7c43fef1b59fe9d1fef47dc395b3a2b
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-11-20 19:00:30 +0000

    Add ability to cycle through zombies and increase the size of the selected one

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class GameManager : MonoBehaviour {
+
+	private int selectedZombiePosition = 0;
+	public GameObject selectedZombie;
+	public List<GameObject> zombies;
+	public Vector3 selectedSize;
+	public Vector3 defaultSize;
+
+	private void Start () {
+		SelectZombie (selectedZombie);
+	}
+
+	private void Update () {
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			GetZombieLeft ();
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			GetZombieRight ();
+		}
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+
+		}
+	}
+
+	private void SelectZombie (GameObject newZombie) {
+		selectedZombie.transform.localScale = defaultSize;
+		selectedZombie = newZombie;
+		newZombie.transform.localScale = selectedSize;
+	}
+
+	private void GetZombieLeft () {
+		if (selectedZombiePosition == 0) {
+			selectedZombiePosition = 3;
+		} else {
+			selectedZombiePosition--;
+		}
+		SelectZombie (zombies [selectedZombiePosition]);
+	}
+
+	private void GetZombieRight () {
+		if (selectedZombiePosition == 3) {
+			selectedZombiePosition = 0;
+		} else {
+			selectedZombiePosition++;
+		}
+		SelectZombie (zombies [selectedZombiePosition]);
+	}
+}
