587b0a750fd78336c9be1d74eb07b4cc1534c7c4
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-11-20 20:35:03 +0000

    Add bgm and sfx

--- Assets/scripts/Tile.cs
+++ Assets/scripts/Tile.cs
@@ -4,8 +4,15 @@ using System.Collections;
 public class Tile : MonoBehaviour {
 
 	public GameManager gameManager;
+	public AudioClip hit;
+	private AudioSource source;
+
+	private void Start () {
+		source = GetComponent<AudioSource> ();
+	}
 
 	private void OnTriggerEnter (Collider other) {
 		gameManager.AddPoint ();
+		source.PlayOneShot (hit);
 	}
 }
